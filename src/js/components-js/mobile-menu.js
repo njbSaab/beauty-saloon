b471293dccbc1d57c0c04a4f306425fb74611e5d
Admin <1q.expert@gmail.com>
2024-05-30 13:49:59 +0300

    done all without optimization

--- src/js/components-js/mobile-menu.js
+++ src/js/components-js/mobile-menu.js
@@ -1,47 +1,12 @@
-// class Navbar {
-//     constructor() {
-//       this.navBar = document.getElementById('nav-bar');
-//       this.menuTrigger = document.querySelector('.menu-trigger');
-//       this.overlayBlur = document.querySelector('.overlay-blur');
-//       this.navBarLinks = document.querySelectorAll('.overlay-content .nav-bar-link');
-//       this.bindEvents();
-//     }
-  
-//     bindEvents() {
-//       this.navBarLinks.forEach((link) => {
-//         link.addEventListener('click', () => this.toggleNavbarActive());
-//       });
-  
-//       this.menuTrigger.addEventListener('click', () => this.toggleNavbarActive());
-//     }
-  
-//     toggleNavbarActive() {
-//       this.menuTrigger.classList.toggle('active');
-//       this.overlayBlur.classList.toggle('active');
-  
-//       if (this.navBar.style.display === "none" || this.navBar.style.display === "") {
-//         this.navBar.style.display = "block";
-//         this.navBar.style.opacity = "1";
-//       } else {
-//         this.navBar.style.display = "none";
-//         this.navBar.style.opacity = "0";
-//       }
-  
-//       this.navBar.style.transition = "opacity 2s ease-in";
-//     }
-//   }
-  
-//   document.addEventListener('DOMContentLoaded', function () {
-//     new Navbar();
-//   });
-  
 
 class Navbar {
   constructor() {
+    this.header = document.querySelector('.header')
     this.ukNavBar = document.querySelector('.uk-navbar');  // Верхний navbar
     this.navBar = document.querySelector('.overlay');      // Оверлейное меню
     this.menuTriggerDesktop = document.querySelector('[data-nav-desktop="true"]');
     this.menuTriggerMobile = document.querySelector('[data-nav-mobile="true"]');
+  
     this.bindEvents();
   }
 
@@ -67,7 +32,8 @@ class Navbar {
     } else if (type === 'mobile') {
       // Переключение класса active на кнопке mobile и navBar
       this.menuTriggerMobile.classList.toggle('active');
-      this.navBar.classList.toggle('active');  // управление оверлейным navBar, а не ukNavBar
+      this.navBar.classList.toggle('active'); 
+      this.header.classList.toggle('active'); 
     }
 
     // Обновление стилей видимости navBar
